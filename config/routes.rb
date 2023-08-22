0516311b01268b6f02e4f46739e59cc3bc4f72ef
Marissa vonWolff <marissa.wolff@gmx.de>
2023-08-22 15:59:12 +0200

    changes with resourcC

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#indexÄ
-  get 'tasks', to: 'tasks#index', as: :tasks
-  get 'tasks/new', to: 'tasks#new', as: :new
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy'
+# Defines the root path route ("/")
+# root "articles#indexÄ
+  resources :tasks
 end
